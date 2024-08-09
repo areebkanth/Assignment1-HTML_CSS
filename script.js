943e6a35695df8803918f76b7c9a0b5e27ebd832
Areeb Kanth <121720093+areebkanth@users.noreply.github.com>
2024-08-09 16:51:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-window.addEventListener('scroll', function() {
-const navbar = document.getElementById('navbar');if (window.scrollY > 50) {navbar.style.backgroundColor = 'black'; 
-    } else {navbar.style.backgroundColor = 'orange'; 
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar'); if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'black';
+    } else {
+        navbar.style.backgroundColor = 'orange';
     }
 });
